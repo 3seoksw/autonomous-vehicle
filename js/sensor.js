95792da2a0a9727ada0446f18392e8ba8bf50332
WooSeok Kim <3suksw@gmail.com>
2023-03-21 15:39:08 +0900

    declarable sensor with ray count

--- js/sensor.js
+++ js/sensor.js
@@ -1,7 +1,7 @@
 class Sensor {
-    constructor(car) {
+    constructor(car, rayCount = 5) {
         this.car = car
-        this.rayCount = 5
+        this.rayCount = rayCount
         this.rayLength = 150
         this.raySpread = Math.PI / 2
 
